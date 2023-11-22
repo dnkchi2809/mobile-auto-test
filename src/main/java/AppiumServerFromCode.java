f3ccc665598f82169a47c1bfafb81d034d1814d7
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2023-11-22 17:25:29 +0700

    fix: login to app

--- src/main/java/AppiumServerFromCode.java
+++ src/main/java/AppiumServerFromCode.java
@@ -1,12 +1,15 @@
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
+
 
 public class AppiumServerFromCode {
 
@@ -43,7 +46,7 @@ public class AppiumServerFromCode {
     }
 
     public static void usingFingerprint() throws InterruptedException, IOException {
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         Runtime.getRuntime().exec("adb -e emu finger touch 1");
     }
 
@@ -53,39 +56,66 @@ public class AppiumServerFromCode {
         }
     }
 
+    protected static void swipeToLeft(AppiumDriver appiumDriver){
+        Dimension windowSize = appiumDriver.manage().window().getSize();
+        int screenWidth = windowSize.getWidth();
+        int point = 90 * screenWidth / 100;
+
+        ((JavascriptExecutor) appiumDriver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+                "left", 0, "top", point, "width", point, "height", 0,
+                "direction", "left",
+                "percent", 0.75
+        ));
+    }
+
     public static void loginZiiChat() throws IOException, InterruptedException {
+        Thread.sleep(3000);
+        swipeToLeft(driver);
+        Thread.sleep(2000);
+        swipeToLeft(driver);
+
+
+        Thread.sleep(4000);
         driver.findElement(By.id("com.ziichat.android.media:id/buttonContinue")).click();
 
+        Thread.sleep(2000);
         driver.findElement(By.id("com.ziichat.android.media:id/btn_get_started")).click();
 
+        Thread.sleep(3000);
         driver.findElement(By.id("com.ziichat.android.media:id/txtUsername")).sendKeys("automobile.halome.0001");
 
+        Thread.sleep(2000);
         driver.findElement(By.id("com.ziichat.android.media:id/btnContinue")).click();
 
-        if(driver.findElement(By.id("com.ziichat.android.media:id/alertTitle")).isDisplayed()){
-            WebElement welcomeBackTitle = (WebElement) driver.findElements(By.id("com.ziichat.android.media:id/alertTitle"));
-            String titleText = welcomeBackTitle.getText();
+        Thread.sleep(2000);
+        if (driver.findElement(By.id("com.ziichat.android.media:id/alertTitle")).isDisplayed()) {
+            driver.findElement(By.id("android:id/button1")).click();
 
-            if(titleText.contains("Welcome back")){
-                driver.findElement(By.id("android:id/button1")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.id("om.google.android.gms:id/continue_button")).click();
 
-                usingFingerprint();
-            }
+            usingFingerprint();
         }
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        AppiumServerFromCode.startAppiumServer();
+        try {
+            AppiumServerFromCode.startAppiumServer();
 
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setAutomationName("uiautomator2")
-                .setAppPackage("com.ziichat.android.media")
-                .setAppActivity("com.halome.media.app.MainActivity");
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),options );
+            UiAutomator2Options options = new UiAutomator2Options();
+            options.setAutomationName("uiautomator2")
+                    .setAppPackage("com.ziichat.android.media")
+                    .setAppActivity("com.halome.media.app.MainActivity");
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"),options );
 
-        unlockLockScreenByFingerprint();
+            unlockLockScreenByFingerprint();
 
-        AppiumServerFromCode.stopAppiumServer();
-        System.out.println("finish");
+            Thread.sleep(4000);
+            loginZiiChat();
+        } finally {
+            AppiumServerFromCode.stopAppiumServer();
+            System.out.println("finish");
+        }
+
     }
 }
