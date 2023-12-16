d5e45d070580959f16984787c53396cf9055ca53
Kim Chi <45139505+dnkchi2809@users.noreply.github.com>
2023-12-16 15:53:03 +0700

    fix: change test flow to 2 threads

--- src/main/java/Flow/TestFlow.java
+++ src/main/java/Flow/TestFlow.java
@@ -4,6 +4,7 @@ import Flow.Search.Search;
 import io.appium.java_client.AppiumDriver;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import static Flow.Chat.Chat.*;
 import static Flow.Login.Login.loginZiiChat;
@@ -12,21 +13,62 @@ import static Flow.Search.Search.goToSearchScreen;
 
 public class TestFlow {
     public static void testFlow(AppiumDriver driver1, AppiumDriver driver2) throws IOException, InterruptedException {
-        loginZiiChat(driver1, "chi.0001");
-        loginZiiChat(driver2, "chi.0002");
+        Thread device1Thread = new Thread(() -> {
+            try {
+                loginZiiChat(driver1, "chi.0001");
 
-        goToSearchScreen(driver1);
-        Search.searchWithKeyword(driver1, "chi.0002");
-        clickOnSearchResult(driver1, 0);
+                goToSearchScreen(driver1);
+                Search.searchWithKeyword(driver1, "chi.0002");
+                clickOnSearchResult(driver1, 0);
 
-        clickEditorInput(driver1);
-        sendMessage(driver1, "device 1 send");
+                clickEditorInput(driver1);
+                sendMessage(driver1, "device 1 send");
 
-        clickEnterChannel(driver2, 0);
-        clickEditorInput(driver2);
-        sendMessage(driver2, "device 2 send");
+                clickBackToChannelList(driver1);
+            } catch (IOException | InterruptedException e){
+                throw new RuntimeException(e );
+            }
+        });
 
-        clickBackToChannelList(driver1);
-        clickBackToChannelList(driver2);
+        Thread device2Thread = new Thread(() -> {
+            try {
+                loginZiiChat(driver2, "chi.0002");
+
+                clickEnterChannel(driver2, 0);
+                clickEditorInput(driver2);
+                sendMessage(driver2, "device 2 send");
+
+                clickBackToChannelList(driver2);
+            } catch (IOException | InterruptedException e){
+                throw new RuntimeException(e );
+            }
+        });
+
+        device1Thread.start();
+        device2Thread.start();
+
+        try {
+            device1Thread.join();
+            device2Thread.join();
+        } catch (InterruptedException e){
+            e.printStackTrace(System.out);
+        }
+
+//        loginZiiChat(driver1, "chi.0001");
+//        loginZiiChat(driver2, "chi.0002");
+//
+//        goToSearchScreen(driver1);
+//        Search.searchWithKeyword(driver1, "chi.0002");
+//        clickOnSearchResult(driver1, 0);
+//
+//        clickEditorInput(driver1);
+//        sendMessage(driver1, "device 1 send");
+//
+//        clickEnterChannel(driver2, 0);
+//        clickEditorInput(driver2);
+//        sendMessage(driver2, "device 2 send");
+//
+//        clickBackToChannelList(driver1);
+//        clickBackToChannelList(driver2);
     }
 }
